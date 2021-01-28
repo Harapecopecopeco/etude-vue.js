a18d1e3c275f83fd9c8cd3a63ab2851b3662b388
Yusuke Watanabe <teamphie@gmail.com>
2021-01-28 16:48:10 +0900

    Create Lost Article

---
+++ etude/src/LikeNumber.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <p>いいね({{ number }})</p>
+  <button @click='inclement'>+1</button>
+    </div>
+</template>
+
+<script>
+export default{
+  dara() {
+    return {
+      number:5
+    };
+  },
+  methods:{
+      inclement(){
+          this.number+=1;
+      }
+  }
+};
+</script>
